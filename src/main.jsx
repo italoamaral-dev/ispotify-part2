0a64ef645a722402a67d50c074b1b611bbd8b9ea
Victor Mafia Araujo <victor.mafiaraujo@gmail.com>
2025-09-06 22:51:47 -0300

    adiciona api

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import Login from "./pages/login.jsx";
 import Signup from "./pages/Signup.jsx";
+import MainPage from "./pages/MainPage.jsx";
 import "./index.css";
 
 const router = createBrowserRouter([
   { path: "/", element: <Navigate to="/login" replace /> },
   { path: "/login", element: <Login /> },
   { path: "/signup", element: <Signup /> },
+  { path: "/mainpage", element: <MainPage />},
 ]);
 
 createRoot(document.getElementById("root")).render(
